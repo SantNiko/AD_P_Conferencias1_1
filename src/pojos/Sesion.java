2558bee61fb4bba52d874cb0cf808bb4f710adfb
Niko <Niko@192.168.1.35>
2017-02-24 22:22:55 +0100

    POJOS Created, missing implementation Contino con la implementacion 	A_Persona 	Autor

--- src/pojos/Sesion.java
+++ src/pojos/Sesion.java
@@ -5,10 +5,100 @@
  */
 package pojos;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Niko
  */
 public class Sesion {
+    
+    private int id;
+    private String nombre;
+    private String info;
+    private Date fechaHora;
+    private Articulo articulo;
+
+    public Sesion() {
+    }
+
+    public Sesion(int id, String nombre, String info, Date fechaHora, Articulo articulo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.info = info;
+        this.fechaHora = fechaHora;
+        this.articulo = articulo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sesion other = (Sesion) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.info, other.info)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaHora, other.fechaHora)) {
+            return false;
+        }
+        if (!Objects.equals(this.articulo, other.articulo)) {
+            return false;
+        }
+        return true;
+    }
+
+    public Date getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(Date fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
     
 }
